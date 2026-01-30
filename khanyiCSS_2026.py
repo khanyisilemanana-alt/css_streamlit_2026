29f43a36f1907b6254d24f43d8eaa8adab4a685d
Khanyisile Manana <khanyisilemanana@gmail.com>
2026-01-30 12:26:49 +0200

    Add files via upload
    Streamlit App assignment Day 3

---
+++ khanyiCSS_2026.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 29 15:16:46 2026
+
+@author: khany
+"""
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Khanyisile Manana")
+st.write("Overview: Researcher. Experienced Medical Technologist, skilled in managing personnel, resources and schedules to handle service demands. Coordinates team activities and trainings to continuously update knowledge and skills. ")
+
+st.write("Field of Research: Biomedical Sciences")
+
+st.write("Institution: University of Johannesburg")
+
+
+st.image("molecular.jpg")
+
+
+st.title("Contact details")
+st.write("You can reach me on")
+st.write("Tel. 011 933 0104")
+st.write("khanyisilemanana@gmail.com")
